965d5e9e72fc4e69558bbc23a6a4c0c8049bcb5f
Jaejuna <anayana9988@gmail.com>
2023-05-30 10:40:44 +0900

    edited

--- MM/gesture_gluon.py
+++ MM/gesture_gluon.py
@@ -1,7 +1,15 @@
 from autogluon.multimodal import MultiModalPredictor
 from autogluon.core.utils.loaders import load_zip
+from autogluon.timeseries import TimeSeriesDataFrame, TimeSeriesPredictor
 
 
+#### time series
+data = TimeSeriesDataFrame('https://autogluon.s3.amazonaws.com/datasets/timeseries/m4_hourly/train.csv')
+
+predictor = TimeSeriesPredictor(target='target', prediction_length=48).fit(data)
+predictions = predictor.predict(data)
+
+#### multimodal 
 data_zip = "https://automl-mm-bench.s3.amazonaws.com/object_detection_dataset/" + "tiny_motorbike_coco.zip"
 load_zip.unzip(data_zip, unzip_dir=".")
 
